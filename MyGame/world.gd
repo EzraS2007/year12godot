e3fda3159570c988d340cfa533da32d13b3ac9ca
EzraS2007 <110065490+EzraS2007@users.noreply.github.com>
2024-06-20 13:22:36 +1200

    Fixed movement system, added collision, worked on map

--- MyGame/world.gd
+++ MyGame/world.gd
@@ -1,23 +1,21 @@
 extends Node3D
 
+
+
+#func _unhandled_input(event):
+	## Example of using mouse input events
+	#if event is InputEventMouseMotion:
+		## Example of accessing mouse motion
+		#var motion = event as InputEventMouseMotion
+		#print("Mouse motion: ", motion.relative)
+#
+	#elif event is InputEventMouseButton:
+		## Example of mouse button press/release
+		#var button = event as InputEventMouseButton
+		#if button.button_index == MOUSE_BUTTON_LEFT and button.pressed:
+			#print("Left mouse button pressed")
+		#elif button.button_index == MOUSE_BUTTON_RIGHT and button.pressed:
+			#print("Right mouse button pressed")
+
 func _ready():
-	# Capture the mouse cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-	# Optionally hide the mouse cursor
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
-func _input(event):
-	# Example of using mouse input events
-	if event is InputEventMouseMotion:
-		# Example of accessing mouse motion
-		var motion = event as InputEventMouseMotion
-		print("Mouse motion: ", motion.relative)
-
-	elif event is InputEventMouseButton:
-		# Example of mouse button press/release
-		var button = event as InputEventMouseButton
-		if button.button_index == MOUSE_BUTTON_LEFT and button.pressed:
-			print("Left mouse button pressed")
-		elif button.button_index == MOUSE_BUTTON_RIGHT and button.pressed:
-			print("Right mouse button pressed")
