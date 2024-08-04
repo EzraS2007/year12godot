e451cb6848dda2e3e0e5b06bde38d17b481c974c
EzraS2007 <110065490+EzraS2007@users.noreply.github.com>
2024-08-05 09:40:18 +1200

    worked on map and weapon

--- MyGame/MODELS/bible.gd
+++ MyGame/MODELS/bible.gd
@@ -1,20 +1,16 @@
-extends CanvasLayer
+extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
-
-func _on_quit_pressed():
-	get_tree().quit()
-
-
-func _on_resume_pressed():
-	get_tree().change_scene_to_file("res://SCENES/world.tscn")
+func _attack():
+	if Input.is_action_just_pressed("attack"):
+		$AttackAnimation.play("swing")
